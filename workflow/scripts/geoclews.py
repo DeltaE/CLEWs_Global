1ffd2e538df7adc544e71b0506f1de741f5ee030
junohBede <jka358@sfu.ca>
2025-07-04 01:46:29 -0700

    placeholders

---
+++ workflow/scripts/geoclews.py
@@ -0,0 +1,12 @@
+import os
+from pathlib import Path
+ROOT_DIR = Path(__file__).parent.parent.parent
+
+
+def main():
+    os.system("conda activate GeoCLEWs")
+    os.system(f"python {ROOT_DIR}/submodules/CLEWs_GAEZ/GAEZ_Processing/main.py")
+
+
+if __name__ == '__main__':
+    main()
