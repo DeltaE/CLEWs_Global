490a846c7638387b72e632686121997367f1ada3
junohBede <jka358@sfu.ca>
2025-07-08 01:25:42 -0700

    config file

--- workflow/scripts/main.py
+++ workflow/scripts/main.py
@@ -5,7 +5,8 @@ ROOT_DIR = Path(__file__).parent.parent.parent
 
 
 def main():
-    os.chdir(f'{ROOT_DIR}')
+    os.chdir(f'{ROOT_DIR}/submodules/osemosys_global')
+    os.system("snakemake -j 10")
 
 
 if __name__ == '__main__':
