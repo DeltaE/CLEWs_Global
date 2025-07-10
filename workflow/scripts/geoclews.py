20457956ef7eacb2d9f92be18979760516d923a9
junohBede <jka358@sfu.ca>
2025-07-09 17:08:35 -0700

    config file

--- workflow/scripts/geoclews.py
+++ workflow/scripts/geoclews.py
@@ -4,7 +4,7 @@ ROOT_DIR = Path(__file__).parent.parent.parent
 
 
 def main():
-    os.system(f"python {ROOT_DIR}/submodules/CLEWs_GAEZ/GAEZ_Processing/main.py")
+    os.system(f"python {ROOT_DIR}/workflow/submodules/CLEWs_GAEZ/GAEZ_Processing/main.py")
 
 
 if __name__ == '__main__':
